8147a378af87c37f1ccefc6a74126e4c1406313c
Danylo <danylokam@gmail.com>
2024-12-05 15:02:36 +0100

    change

--- src/SnapRecall.Api/Program.cs
+++ src/SnapRecall.Api/Program.cs
@@ -5,54 +5,45 @@ using SnapRecall.Application.SnapRecallBotHandlers;
 using SnapRecall.Infrastructure.Data;
 using Telegram.BotAPI;
 
-namespace SnapRecall.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<TelegramSettings>(
+    builder.Configuration.GetSection(TelegramSettings.SectionName));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ITelegramBot, SnapRecallBot>();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>((serviceProvider) =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
 
-            // Add services to the container.
+    return new TelegramBotClient("7744751662:AAE1wcsbF_RGEln5MpMd582oxiKgVtqwxvc", httpClient);
+});
 
-            builder.Services.Configure<TelegramSettings>(
-                builder.Configuration.GetSection(TelegramSettings.SectionName));
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddTransient<ITelegramBot, SnapRecallBot>();
-            builder.Services.AddHttpClient();
-            builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>((serviceProvider) =>
-            {
-                var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
-
-                return new TelegramBotClient("7744751662:AAE1wcsbF_RGEln5MpMd582oxiKgVtqwxvc", httpClient);
-            });
-
-            builder.Services.AddSingleton(x => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
+builder.Services.AddSingleton(x => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
 
 
-            builder.Services.AddDbContext<SnapRecallDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<SnapRecallDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            app.UseSwagger();
-            app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-            app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+app.UseAuthorization();
 
 
-            app.MapControllers();
+app.MapControllers();
 
-            app.UseTelegramWebhook();
-            app.Run();
-        }
-    }
-}
+app.UseTelegramWebhook();
+app.Run();
